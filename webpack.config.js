d86f6c43ae1f2d9a4fb9479093b4bba9df32573d
martinezmauri <maurimartine01@gmail.com>
2024-10-09 14:48:29 -0300

    agregando configuracion de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./scripts/index.js",
   output: {
     path: __dirname + "/public",
     filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+  ],
   resolve: {
     extensions: [".js"],
   },
